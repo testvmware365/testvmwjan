bff9f5159c0bebec5cb3ca0011f4fca7cd73b7dd
shreyashi <shreyapallu@gmail.com>
2020-01-14 11:38:28 +0530

    Update file_write.py

--- file_write.py
+++ file_write.py
@@ -5,13 +5,6 @@ path = '/Users/shashis/Desktop/py_train/abc.txt'
 
 fo=open(path)
 fo1=open('log_connect.txt','a')
-data=fo.readlines()
 
-for record in data:
-	result=os.system('ping -c 1 {} > null'.format(record.split(',')[1]))
-	if result == 0:
-		print ("{} is connected".format(record.split(',')[1]))
-	else:
-		print ("{} is not connected".format(record.split(',')[1]))
 fo.close()
 fo1.close()
